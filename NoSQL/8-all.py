c28a8a335c4b3d770415e2a40936f8d8550ba9a4
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-30 23:29:59 +0400

    Update 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""list qaaa"""
+""" List documents """
 import pymongo
 
 
-def list_all(mango_collection) -> list:
-    """function do some thing"""
-    
+def list_all(mongo_collection) -> list:
+    """ Lists all documents in a collection
+        Args:
+            mongo_collection: Collection of object
+
+        Return:
+            List with documents, otherwise []
+    """
     documents: list = []
-    
-    
-    for documnet in mongo_collection.find():
-        documnets.append(document)
-    
-    return documnets
+
+    for document in mongo_collection.find():
+        documents.append(document)
+
+    return documents
