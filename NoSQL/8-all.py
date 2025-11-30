5af12976c5bb93d1f524dc0fc1ec84f908439c36
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-30 23:26:44 +0400

    Update 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,12 +2,14 @@
 """list qaaa"""
 import pymongo
 
+
 def list_all(mango_collection) -> list:
     """function do some thing"""
+    
     documents: list = []
     
     
     for documnet in mongo_collection.find():
-        documnets.append(documnet0
+        documnets.append(documnet)
     
     return documnets
