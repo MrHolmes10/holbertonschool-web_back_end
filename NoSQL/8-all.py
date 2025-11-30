5c50139830ac8b27e702feefbfe195c565a33016
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-30 15:51:24 +0000

    ff

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""list qaaa"""
+import pymongo
+
+def list_all(mango_collection) -> list:
+    """function do some thing"""
+    documents: list = []
+    
+    
+    for documnet in mongo_collection.find():
+        documnets.append(documnet0
+    
+    return documnets
